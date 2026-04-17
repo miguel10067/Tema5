c9fc85251f7798148868b472b139852b9cc7e1e3
miguel10067 <miguelcampara1@gmail.com>
2026-04-17 16:27:40 +0200

    Ejercicio5 test

--- src/main/java/Bisiesto.java
+++ src/main/java/Bisiesto.java
@@ -2,8 +2,7 @@ public class Bisiesto {
         public static boolean esBisiesto(int anyo) {
             if ((anyo % 4 == 0 && anyo % 100 != 0) || (anyo % 400 == 0)) {
                 return true;
-            } else {
-                return false;
+            } else { return false;
             }
         }
     }
--- src/main/java/BisiestoTest.java
+++ src/main/java/BisiestoTest.java
@@ -4,10 +4,10 @@ public class BisiestoTest {
     @Test
         void testEsBisiesto() {
             assertAll(
-                    () -> assertEquals(Bisiesto.esBisiesto(2024), true, "2024 es bisiesto"),
-                    () -> assertFalse(Bisiesto.esBisiesto(1900), "1900 No es bisiesto"),
-                    () -> assertTrue(Bisiesto.esBisiesto(2000), "2000 es bisiesto"),
-                    () -> assertFalse(Bisiesto.esBisiesto(2023), "2023 No es bisiesto")
+                    () -> assertEquals(Bisiesto.esBisiesto(2024), true, "2024 es bisiesto."),
+                    () -> assertFalse(Bisiesto.esBisiesto(1900), "1900 No es bisiesto."),
+                    () -> assertTrue(Bisiesto.esBisiesto(2000), "2000 es bisiesto."),
+                    () -> assertFalse(Bisiesto.esBisiesto(2023), "2023 No es bisiesto.")
             );
         }
     }
